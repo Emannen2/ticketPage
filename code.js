231e0931f4ae921ff397e7b7f6440995084ebe99
SimonMyrvold <simon.johan.myrvold@gmail.com>
2023-03-08 13:25:48 +0100

    navbar hamburger

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function darkModeToggle() {
-    document.getElementById('bgb').className('pl-3 pt-3 w-full h-16 flex bg-black').toggle
+    document.getElementById('bgb').classList.toggle('bg-black').toggle('bg-white');
+}
+
+function hamburgerToggle() {
+    document.getElementById('hamburgerContent').classList.toggle('active');
+    document.getElementById('hamburgerLine1').classList.toggle('active');
+    document.getElementById('hamburgerLine2').classList.toggle('active');
+    document.getElementById('hamburgerLine3').classList.toggle('active');
+    document.getElementById('body').classList.toggle('active');
 }
