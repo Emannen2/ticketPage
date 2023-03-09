e443e7e9cd64d15952b99c3fae268fed45a1de67
SimonMyrvold <simon.johan.myrvold@gmail.com>
2023-03-09 11:26:44 +0100

    fixed navbar and hamburger menu

--- code.js
+++ code.js
@@ -9,3 +9,17 @@ function hamburgerToggle() {
     document.getElementById('hamburgerLine3').classList.toggle('active');
     document.getElementById('body').classList.toggle('active');
 }
+
+addEventListener('resize', function() {
+    if (window.innerWidth > 1370) {
+        document.getElementById('hamburgerContent').classList.remove('active');
+        document.getElementById('hamburgerLine1').classList.remove('active');
+        document.getElementById('hamburgerLine2').classList.remove('active');
+        document.getElementById('hamburgerLine3').classList.remove('active');
+        document.getElementById('body').classList.remove('active');
+    }
+});
+
+document.getElementById('textInput').addEventListener('keypress', function(e) {
+    //be be worked on
+});
